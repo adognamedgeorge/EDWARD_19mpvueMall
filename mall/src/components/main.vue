24a56ccc67422222dd8ceeab6ec582cb2af0b8b7
edward <1483500659@qq.com>
2019-04-25 16:51:40 +0800

    April25

--- mall/src/components/main.vue
+++ mall/src/components/main.vue
@@ -1,34 +1,13 @@
 <template>
   <div id="box">
     <div class="flexColumn">
-      <div>
-        <img src="/static/images/cat.png" alt="">
-      </div>
-      <div>
-        <img src="/static/images/cat.png" alt="">
-      </div>
-      <div>
-        <img src="/static/images/cat.png" alt="">
+      <div v-for="(item, index) in categoryList" :key="index">
+        <img :src="item.url" alt="">
       </div>
     </div>
     <div class="flexRow">
-      <div>
-        <img src="/static/images/cat.png" alt="">
-      </div>
-      <div>
-        <img src="/static/images/cat.png" alt="">
-      </div>
-      <div>
-        <img src="/static/images/cat.png" alt="">
-      </div>
-      <div>
-        <img src="/static/images/cat.png" alt="">
-      </div>
-      <div>
-        <img src="/static/images/cat.png" alt="">
-      </div>
-      <div>
-        <img src="/static/images/cat.png" alt="">
+      <div v-for="(item, index) of newCategory" :key="index">
+        <img :src="item.url" alt="">
       </div>
     </div>
   </div>
@@ -36,7 +15,40 @@
 
 <script>
 export default {
-  name: 'homeMain'
+  name: 'homeMain',
+  data () {
+    return {
+      categoryList: [
+        { url: 'https://goss.veer.com/creative/vcg/veer/612/veer-134669323.jpg' },
+        { url: 'https://goss2.veer.com/creative/vcg/veer/612/veer-133071017.jpg' },
+        { url: 'https://goss.veer.com/creative/vcg/veer/612/veer-147395960.jpg' }
+      ],
+      newCategory: [
+        { url: 'https://goss4.veer.com/creative/vcg/veer/612/veer-161959036.jpg' },
+        { url: 'https://goss.veer.com/creative/vcg/veer/612/veer-134669323.jpg' },
+        { url: 'https://goss2.veer.com/creative/vcg/veer/612/veer-133071017.jpg' },
+        { url: 'https://goss.veer.com/creative/vcg/veer/612/veer-147395960.jpg' },
+        { url: 'https://goss.veer.com/creative/vcg/veer/612/veer-134669323.jpg' },
+        { url: 'https://goss2.veer.com/creative/vcg/veer/612/veer-133071017.jpg' }
+      ]
+    }
+  },
+  methods: {
+    getList () {
+      let Fly = require('flyio')
+      let fly = new Fly()
+      fly.get('https://easy-mock.com/mock/5ca466b55eeed03805bf4949/edward/edward')
+        .then((res) => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  },
+  mounted () {
+    this.getList()
+  }
 }
 </script>
 
@@ -56,7 +68,6 @@ export default {
     }
   }
   .flexRow {
-    background-color: yellow;
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
